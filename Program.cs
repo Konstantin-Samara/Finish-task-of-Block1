f74577b3d549cc524efcdf5f7afcf1b4ab0e0481
Konstantin <kons1979@mail.ru>
2022-09-17 01:27:36 +0400

    task finished ver.2

--- Program.cs
+++ Program.cs
@@ -2,16 +2,17 @@
 int Length = Convert.ToInt32(Console.ReadLine());
 if (Length>0) {
     string[] array1 = new string[Length];
-    string[] array2 = new string[Length];
-    int n=0;
+        int n=0;
     for (int i=0; i<Length; i++) {
         Console.Write($"Введите элемент массива с индексом {i} : ");
         array1[i]=Console.ReadLine();
-        if (array1[i].Length<4) {
-            array2[n]=array1[i]; 
-            n++;}    }
+        if (array1[i].Length<4) {n++;}}
     if (n==0) 
     {Console.WriteLine("В заданном массиве нет элементов, содержащих <=3-х символов.");}
     else {
+        string[] array2 = new string[n];
+        int j = 0;
+        for (int i=0; i<Length; i++) {
+                if (array1[i].Length<4) {array2[j]=array1[i];j++;}}
         for (int i=0; i<n; i++) {Console.WriteLine(array2[i]);}}}
 else {Console.WriteLine("кол-во элементов (длинна) массива должна быть > 0");}
